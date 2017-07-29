5eeb09ea318e4356a67b7ab28e9cbf677cd74d33
Brett Fuller <bfuller123@gmail.com>
2017-07-28 23:24:16 -0500

    fixed to serve css

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/index.html"));
 })
 
 app.listen(PORT, function() {
