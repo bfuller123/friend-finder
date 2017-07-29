6bd9ec34d198dbf0e4c7ff97b0d4b8cecdbe6ba6
Brett Fuller <bfuller123@gmail.com>
2017-07-28 23:09:02 -0500

    added logic to start server

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
+app.listen(PORT, function() {
+  console.log(`Server listening on port ${PORT}`);
+})
